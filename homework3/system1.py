c72cdeb321d55bd7f0be430f352e001de7207934
satikpro <saturnpro73@gmail.com>
2022-01-15 04:02:06 +0200

    homework-4

--- homework3/system1.py
+++ homework3/system1.py
@@ -1,15 +1,17 @@
 import math
-p = -math.cos(1)**2
+
 
 x = int(input("Введите x "))
 if x > (-math.pi) and x < math.pi/4:
-    y = p*x + math.cos(1)**2 * math.pi
+    y = -math.cos(x -math.pi)**2
  
 elif x >= math.pi/4 and x <=1:
     module = math.fabs(x+1)
     y = math.sqrt({module})
-else:
+elif x>1:
     y = 1/x-1
+else:
+   y =  print("Error")
     
 print(y)
 
