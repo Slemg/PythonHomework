8dd5805f61f64b3c23e26321caba7af4c6d31ef4
satikpro <saturnpro73@gmail.com>
2022-01-29 02:15:21 +0200

    first commit

---
+++ homework8/task1.py
@@ -0,0 +1,5 @@
+animals = ['dog', 'cat', 'hamster']
+for i in animals:
+  print(i)
+print(f"this {animals[0]} is cute")
+print(f"Any of these animals are cute!")
---
+++ homework8/task3.py
@@ -0,0 +1,4 @@
+guests = ['guest1', 'guest2', 'guest3']
+print(guests)
+dlina = len(guests)
+print(f"количество гостей: {dlina}")
