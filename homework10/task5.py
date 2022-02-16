14916a3760b6f4939c7e882c8694c15e012ff0f7
satikpro <saturnpro73@gmail.com>
2022-02-16 22:28:18 +0200

    first commit

--- homework10/task5.py
+++ homework10/task5.py
@@ -1,1 +1,6 @@
-task5.py
+def describe_city(city, country="Netherlands"):
+    print(f"{city} is in {country}")
+
+describe_city("Kyiv", "Ukraine")
+describe_city("Neapol", "Italy")
+describe_city("Amsterdam")
